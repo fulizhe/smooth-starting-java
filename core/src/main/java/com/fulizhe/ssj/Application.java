f72b977faae8515d68e5b83617ef0bd14e59ef27
luoqi <184759758@qq.com>
2024-12-06 16:51:39 +0800

    init commit. 流程跑通

---
+++ core/src/main/java/com/fulizhe/ssj/Application.java
@@ -0,0 +1,39 @@
+package com.fulizhe.ssj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import cn.hutool.core.date.StopWatch;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        final StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+
+        applicationRun(args);// 初始化Spring容器并启动
+
+        stopWatch.stop();
+        log.info("### gis-service启动耗时: {}s", stopWatch.getTotalTimeSeconds());
+    }
+
+    private static void applicationRun(String[] args) {
+        final SpringApplication springApplication = new SpringApplication(Application.class);
+//        final Boolean humanableStart = false;// Convert.toBool(CommonUtil.getProperty("START_HUMANABLE",
+//                                            // "true"));
+//        if (humanableStart) {
+//            springApplication.setApplicationContextFactory(
+//                    new AnnotationConfigServletWebServerApplicationContextWithHumanableStart.Factory());
+//            // springApplication.setMainApplicationClass(
+//            // AnnotationConfigServletWebServerApplicationContextWithHumanableStart.class);
+//            // // 自定义
+//        }
+
+        // 启动Spring容器
+        springApplication.run(args);
+    }
+}
---
+++ core/src/main/java/com/fulizhe/ssj/HelloController.java
@@ -0,0 +1,45 @@
+package com.fulizhe.ssj;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.RandomUtil;
+
+@RestController
+public class HelloController {
+    
+    public HelloController(){
+        ThreadUtil.safeSleep(1000 * 20);
+    }
+
+	@GetMapping("/hello")
+	public String index(HttpServletRequest request) {
+		String randomString = RandomUtil.randomString(5);
+		LoggerFactory.getLogger(this.getClass()).info("接受到参数 {}", "name = " + randomString);
+
+		return "hello";
+	}
+
+	@GetMapping("/hello2")
+	public String helloFail(HttpServletRequest request) {
+		String randomString = RandomUtil.randomString(5);
+		LoggerFactory.getLogger(this.getClass()).info("接受到参数 {}", "name = " + randomString);
+
+		throw new RuntimeException("抛出一异常， 验证flow chart");
+	}
+	
+	@RequestMapping(value = "/", produces = "text/html")
+	public String home() {
+		String appName = "LQ-appName";
+		return "<head><title>Sidecar</title></head><body>\n"
+				+ "<a href='/ping'>ping</a><br/>\n"
+				+ "<a href='/actuator/health'>health</a><br/>\n" + "<a href='/hosts/"
+				+ appName + "'>hosts/" + appName + "</a><br/>\n" + "</body>";
+	}		
+
+}
